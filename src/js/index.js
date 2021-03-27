e94891a1c5e701e72009a1621b60bd65e265156f
Bartłomiej Popiołek <popiolekbartlomiej369@gmail.com>
2021-03-27 16:45:54 +0100

    Task 1 - Styled popup with form, Added custom checkbox and close button, Added logic to close button

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
-/* Here goes your JS code */
+// Get a reference to the buttons and popup elements
+const showPopUpBtn = document.getElementById("show-popup-form");
+const closePopUpBtn = document.getElementById("close-popup-form");
+const popUp = document.querySelector(".popup");
+
+// addEventListener for showing popup
+showPopUpBtn.addEventListener("click", () => {
+    popUp.style.setProperty("display", "flex")
+})
+
+// addEventListener for closing popup
+closePopUpBtn.addEventListener('click', () => {
+    popUp.style.removeProperty("display")
+})
